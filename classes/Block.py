d5fbee5dfd7eac582d61579ef373b3188e731a56
Adam <N00211418@iadt.ie>
2023-06-11 12:20:39 +0100

    changed from node to block (node gives error)

---
+++ classes/Block.py
@@ -0,0 +1,12 @@
+
+class Block:
+    def __init__(self, id, text):
+        self.id = id
+        self.text = text
+    
+    def get_input():
+        input()
+
+    def read_words():
+        print(self.text)
+
