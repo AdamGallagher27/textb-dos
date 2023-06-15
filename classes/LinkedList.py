2c5ec749e6a485c3af6346873c30fad321dd29bd
Adam <N00211418@iadt.ie>
2023-06-15 10:06:58 +0100

    created add at index / prepend

--- classes/LinkedList.py
+++ classes/LinkedList.py
@@ -1,11 +1,9 @@
-
 class LinkedList:
     def __init__(self):
         self.head = None
 
     # add to end of list
     def append(self, block):
-
         # if head is none head = new block
         if self.head is None:
             self.head = block
@@ -16,9 +14,41 @@ class LinkedList:
             # iterate over current till next is none
             while current.next:
                 current = current.next
-            
+
             # next = new block
             current.next = block
+
+    # add block to start of list
+    def prepend(self, block):
+        # if head is none block = head
+        if self.head is None:
+            self.head = block
+        else:
+            # next block is head
+            # head is block
+            block.next = self.head
+            self.head = block
+
+    # add at given index
+    def add_index(self, index, block):
+        if index == 0:
+            self.prepend(block)
+        else:
+            current = self.head
+            prev = None
+            count = 0
+
+            # iterate over list to index
+            while current and count < index:
+                prev = current
+                current = current.next
+                count += 1
+
+            if count == index:
+                # next block is current
+                # previous is block
+                block.next = current
+                prev.next = block
 
     def print_list(self):
         current = self.head
